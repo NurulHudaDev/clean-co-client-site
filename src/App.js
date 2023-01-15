e839c1e4bbdec85bdc2b896c3f8d307f879501b6
NurulHudaDev <nurulhuda22dev@gmail.com>
2023-01-15 23:31:48 +0600

    navbar-style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-
+import { publicRoute } from "./Routes/publicRoutes";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Navbar>
+        <Routes>
+          {
+            publicRoute.map(({path, Component}, index) =>(
+              <Route key={index} path={path} element={<Component />} />
+            ))
+            }
+        </Routes>
+      </Navbar>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,25 +6,29 @@ module.exports = {
   daisyui: {
     themes: [
       {
-        mytheme: {
-        
-"primary": "#176F6B",
-        
-"secondary": "#FFC000",
-        
-"accent": "#FFEDD5",
-        
-"neutral": "#F3F4F6",
-        
-"base-100": "#FFFFFF",
-        
-"info": "#98A8DD",
-        
-"success": "#1BBB70",
-        
-"warning": "#DF7E07",
-        
-"error": "#FA5C5C",
+        light: {
+          "primary": "#176F6B",
+          "secondary": "#FFC000",
+          "accent": "#FFEDD5",
+          "neutral": "#F3F4F6",
+          "base-100": "#FFFFFF",
+          "info": "#98A8DD",
+          "success": "#1BBB70",
+          "warning": "#DF7E07",
+          "error": "#FA5C5C",
+        },
+      },
+      {
+        dark: {
+          "primary": "#D9F99D",
+          "secondary": "#FDE68A",
+          "accent": "#4B5563",
+          "neutral": "#F3F4F6",
+          "base-100": "#1F2937",
+          "info": "#98A8DD",
+          "success": "#1BBB70",
+          "warning": "#F59E0B",
+          "error": "#FB7185",
         },
       },
     ],
